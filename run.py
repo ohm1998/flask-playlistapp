7b4da54613f4334450624d0a2f1908f26f980103
ohm1998 <ohm.1998@gmail.com>
2020-10-07 15:18:12 +0530

    login addition

--- run.py
+++ run.py
@@ -1,15 +1,20 @@
-from flask import Flask, request, jsonify,render_template,redirect,url_for,send_from_directory,session
+from flask import Flask, request,jsonify,render_template,redirect,url_for,send_from_directory,session
 from werkzeug.utils import secure_filename
 import os
 from helpers import *
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super secret'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_DB'] = 'nptel'
 
+mysql = MySQL(app)
 
 UPLOAD_FOLDER = './uploads'
-
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 
 @app.route('/')
@@ -19,6 +24,69 @@ def index():
 	else:
 		session['count'] = 0
 	return 'hello app is running count : ' + str(session['count'])
+
+
+@app.route('/db')
+def db():
+	#print(help(mysql))
+	cur = mysql.connection.cursor()
+	cur.execute("SELECT * FROM event")
+	rv = cur.fetchall()
+	#print(rv)
+	return str(rv)
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+	try:
+		if request.method == 'GET':
+			return render_template('register.html')
+		elif request.method == 'POST':
+			print("post request");
+			username = request.form['username']
+			password = request.form['password']
+			cur = mysql.connection.cursor()
+			query = "INSERT INTO `login`(`username`, `password`) VALUES ('{}','{}')".format(username,password)
+			print(query)
+			cur.execute(query);
+			mysql.connection.commit()
+			#rv = cur.fetchall()
+			return jsonify({
+				"status": "success",
+				"msg" : "user registered successfully"
+				})
+	except Exception as e:
+		print(e)
+		return str(e)
+@app.route('/login',methods=['GET','POST'])
+def login():
+	try:
+		if request.method == 'GET':
+			return render_template('login.html')
+		elif request.method == 'POST':
+			username = request.form['username']
+			password = request.form['password']
+			cur = mysql.connection.cursor()
+			query = "select * from login where username = '{}'".format(username)
+			print(query)
+			cur.execute(query);
+			rv = cur.fetchall()
+			session['user_id'] = rv[0][0]
+			session['username'] = rv[0][1]
+			if(rv[0][2]==password):
+				return jsonify({
+					"status" : "success",
+					"msg" : "login Successfull"
+					})
+			else: 
+				return jsonify({
+					"status": "fail",
+					"msg" : "Credentials Incorrect"
+					})
+	except Exception as e:
+		print(e)
+		return str(e)
+
+
 
 
 @app.route('/upload',methods=['GET','POST'])
